2780a90d2ca2fbab6d754bb81c228982bb58612a
Samyar <samyarkd@Gmail.com>
2023-11-05 14:15:40 +0330

    feat: add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Content from "@/components/content"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Content>
-      <p>This is by far the most hassle free and best way to implement this. It is purely CSS and very very simple to do, no extra files - nothing. Ok, its not the best way possible, but it works very well, very reliable (never failed when testing across very old browsers) and very fast to load.
-
-        Found it a few months ago, and used it ever since, simply copy and paste this code in to your CSS.</p>
+    <Content className="flex flex-col-reverse sm:flex-row gap-4">
+      <Image
+        className="w-full shadow-xl dark:shadow-white/20 shadow-black/10 max-w-xs sm:w-2/4 rounded-lg"
+        width={200}
+        height={200}
+        alt="Samyar Koei"
+        src={'/samyar.jpg'} />
+      <div className="space-y-4">
+        <h2 className="text-4xl">Samyar Koei - <span className="text-2xl text-gray-300">Web Dev 🕸️</span></h2>
+        <p className="col-span-3">This is by far the most hassle free and best way to implement this. It is purely CSS and very very simple to do, no extra files - nothing. Ok, its not the best way possible, but it works very well, very reliable (never failed when testing across very old browsers) and very fast to load.
+          Found it a few months ago, and used it ever since, simply copy and paste this code in to your CSS.</p>
+      </div>
     </Content>
+
   )
 }
--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,9 +1,10 @@
+import clsx from "clsx"
 import { ReactNode } from "react"
 
-const Content = ({ children }: { children: ReactNode }) => {
+const Content = ({ children, className }: { children: ReactNode, className?: string }) => {
   return (
-    <main className="max-w-6xl mx-auto flex flex-col items-center justify-between p-4 sm:p-24 sm:pt-8">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-start text-xl lg:flex">
+    <main className="max-w-6xl mx-auto flex flex-col items-center justify-between p-4 md:p-24 sm:pt-8">
+      <div className={clsx("z-10 max-w-5xl w-full items-center justify-between text-start text-xl lg:flex", className)}>
         {children}
       </div>
     </main>
