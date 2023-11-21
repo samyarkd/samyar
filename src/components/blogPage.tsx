d94fee021e2fb8d7647b09ebbed916853b59bf8d
Samyar <samyarkd@Gmail.com>
2023-11-21 20:47:37 +0330

    fix: tina

--- src/components/blogPage.tsx
+++ src/components/blogPage.tsx
@@ -1,13 +1,12 @@
-// THIS FILE HAS BEEN GENERATED WITH THE TINA CLI.
-// @ts-nocheck
-// This is a demo file once you have tina setup feel free to delete this file
-
-import Head from 'next/head'
+'use client'
 import { useTina } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
-import client from '../../../../tina/__generated__/client'
 
-const BlogPage = (props) => {
+const BlogPage = (props: {
+  query: any,
+  variables: any,
+  data: any,
+}) => {
   const { data } = useTina({
     query: props.query,
     variables: props.variables,
@@ -15,81 +14,34 @@ const BlogPage = (props) => {
   })
 
   return (
-    <>
-      <Head>
-        {/* Tailwind CDN */}
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css'
-          integrity='sha512-y6ZMKFUQrn+UUEVoqYe8ApScqbjuhjqzTuwUMEGMDuhS2niI8KA3vhH2LenreqJXQS+iIXVTRL2iaNfJbDNA1Q=='
-          crossOrigin='anonymous'
-          referrerPolicy='no-referrer'
-        />
-      </Head>
-      <div>
-        <div
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          <h1 className='text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-            {data.post.title}
-          </h1>
-          <ContentSection content={data.post.body}></ContentSection>
-        </div>
-        <div className='bg-green-100 text-center'>
-          Lost and looking for a place to start?
-          <a
-            href='https://tina.io/guides/tina-cloud/getting-started/overview/'
-            className='text-blue-500 underline'
-          >
-            {' '}
-            Check out this guide
-          </a>{' '}
-          to see how add TinaCMS to an existing Next.js site.
-        </div>
+    <div>
+      <div
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        <h1 className='text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+          {data.post.title}
+        </h1>
+        <ContentSection content={data.post.body}></ContentSection>
       </div>
-    </>
-  )
-}
-
-export const getStaticProps = async ({ params }) => {
-  let data = {}
-  let query = {}
-  let variables = { relativePath: `${params.filename}.md` }
-  try {
-    const res = await client.queries.post(variables)
-    query = res.query
-    data = res.data
-    variables = res.variables
-  } catch {
-    // swallow errors related to document creation
-  }
-
-  return {
-    props: {
-      variables: variables,
-      data: data,
-      query: query,
-      //myOtherProp: 'some-other-data',
-    },
-  }
-}
-
-export const getStaticPaths = async () => {
-  const postsListData = await client.queries.postConnection()
-
-  return {
-    paths: postsListData.data.postConnection.edges.map((post) => ({
-      params: { filename: post.node._sys.filename },
-    })),
-    fallback: false,
-  }
+      <div className='bg-green-100 text-center'>
+        Lost and looking for a place to start?
+        <a
+          href='https://tina.io/guides/tina-cloud/getting-started/overview/'
+          className='text-blue-500 underline'
+        >
+          {' '}
+          Check out this guide
+        </a>{' '}
+        to see how add TinaCMS to an existing Next.js site.
+      </div>
+    </div>)
 }
 
 export default BlogPage
 
-const PageSection = (props) => {
+const PageSection = (props: any) => {
   return (
     <>
       <h2>{props.heading}</h2>
@@ -102,7 +54,7 @@ const components = {
   PageSection: PageSection,
 }
 
-const ContentSection = ({ content }) => {
+const ContentSection = ({ content }: any) => {
   return (
     <div className='relative py-16 bg-white overflow-hidden text-black'>
       <div className='hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full'>
@@ -216,3 +168,4 @@ const ContentSection = ({ content }) => {
     </div>
   )
 }
+
