d31dac9412b068408fee5b9fed8108f4330694fb
Samyar <samyarkd@Gmail.com>
2023-11-24 21:14:22 +0330

    feat: sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { tinaclient } from '@/utils/tina'
+import { MetadataRoute } from 'next'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pages = await tinaclient.queries.postConnection()
+  const BASE_URL = "https://samyar.me/"
+
+  return [
+    {
+      url: BASE_URL,
+      priority: 1,
+    },
+    {
+      url: BASE_URL + 'blog',
+      priority: 0.9,
+    },
+    ...(pages.data.postConnection.edges?.map((p) => {
+      return {
+        url: BASE_URL + 'blog/' + p?.node?._sys.filename,
+        priority: 0.6,
+      }
+    }) ?? [])]
+}
