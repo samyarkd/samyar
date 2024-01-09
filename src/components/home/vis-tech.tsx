1734f0db2563f3076bc6cc06b9eb855089ad1281
Samyar <samyarkd@Gmail.com>
2024-01-09 23:34:14 +0330

    style: animate the vis tech title when in view

--- src/components/home/vis-tech.tsx
+++ src/components/home/vis-tech.tsx
@@ -2,18 +2,23 @@ import HomeSection from '@/components/home/home-section'
 import SkillBubble from '@/components/skill-bubble'
 import { Description, SecondaryHeader } from '@/components/ui/typography'
 import { skills } from '@/utils/static-data'
+import { useInView } from 'framer-motion'
+import { useRef } from 'react'
 import { RoughNotation } from 'react-rough-notation'
 
 export default function VisTech() {
+  const headingRef = useRef(null)
+  const isInView = useInView(headingRef)
+
   return (
     <HomeSection className="items-stretch w-full py-16">
-      <div className="text-center space-y-4 pb-4">
+      <div className="text-center space-y-4 pb-4" ref={headingRef}>
         <SecondaryHeader>
           <RoughNotation
             animate
-            show
+            show={isInView}
             type="highlight"
-            animationDelay={2000}
+            animationDelay={100}
             color="#ffff0030"
           >
             Visualized Skill list 👀
