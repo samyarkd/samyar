e01bf3695d4aa6199e91dcc3bd7155b801579144
Samyar <samyarkd@Gmail.com>
2024-01-10 19:54:36 +0330

    fix: module not found net

---
+++ src/components/home/vis-tech-title.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useInView } from 'framer-motion'
+import { useRef } from 'react'
+import { Description, SecondaryHeader } from '../ui/typography'
+import { RoughNotation } from 'react-rough-notation'
+
+export default function VisualizedHeaderTitle() {
+  const headingRef = useRef(null)
+  const isInView = useInView(headingRef)
+
+  return (
+    <div className="text-center space-y-4 pb-4" ref={headingRef}>
+      <SecondaryHeader>
+        <RoughNotation
+          animate
+          show={isInView}
+          type="highlight"
+          animationDelay={100}
+          color="#ffff0030"
+        >
+          Visualized Skill list 👀
+        </RoughNotation>
+      </SecondaryHeader>
+      <Description className="mb-2">
+        The Bigger the skill is the more skilled I&#39;m at it
+      </Description>
+    </div>
+  )
+}
